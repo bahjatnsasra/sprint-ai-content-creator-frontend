a7821cbbeaaa7b12264ac75d278dc3deb6183220
eyal porat <eyal1.porat@gmail.com>
2024-01-21 18:31:02 +0200

    ui changes

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
 import styels from './homepage.module.css'
 
 const homepage = () => {
     return <div className={styels.container}>
-        <button className={styels.btn}>התחלה</button>
-    </div>
+            <img src="/homepage_text.svg" alt="image" />
+            <button className={styels.btn}>התחילו</button>
+
+            <img  className={styels.img} src="/image.svg" alt="image" />
+        </div>
 }
 
 export default homepage
