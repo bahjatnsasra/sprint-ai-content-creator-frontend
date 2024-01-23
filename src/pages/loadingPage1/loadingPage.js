b0fd3cb8eff48008e55d55b83ffdea78e39aa736
eyal porat <eyal1.porat@gmail.com>
2024-01-23 23:24:31 +0200

    ui changes

--- src/pages/loadingPage1/loadingPage.js
+++ src/pages/loadingPage1/loadingPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './loadingPage.module.css';
 import Navbar from '../../components/navbar/navbar';
 
-const LoadingPage = () => {
+const LoadingPage = (props) => {
 
     return (
         <React.Fragment>
@@ -12,7 +12,7 @@ const LoadingPage = () => {
             
                 <div className={styles.box_container}>
                     <img className={styles.loading_gif} src="//2279b9660181ae6c1d8dcced2d7bfdc6.cdn.bubble.io/f1703410484586x240606988286996770/output-onlinegiftools.gif" alt="loading gif" />
-                    <label>אנחנו מכינים לכם מתווה למסלול</label>
+                    <label>אנחנו מכינים לכם רעיונות בתחום ה{props.subject} למסלול</label>
                     <img className={styles.tiny_img} src="/tiny_image.svg" alt="image" />
                 </div>
             
