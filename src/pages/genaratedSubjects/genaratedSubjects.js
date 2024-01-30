3d9ffc0fa45ae852aab50757fcf6214ab291da03
eyal porat <eyal1.porat@gmail.com>
2024-01-30 16:59:47 +0200

    ui ch

--- src/pages/genaratedSubjects/genaratedSubjects.js
+++ src/pages/genaratedSubjects/genaratedSubjects.js
@@ -4,53 +4,34 @@ import Navbar from '../../components/navbar/navbar';
 import UnderButtons from '../../components/underButtons/underButtons'
 import { GoSync } from "react-icons/go";
 import {getSubjects} from '../../service/openAIService'
+import Loading from '../../components/loading/loading';
 
 const GenaratedSubjects = (props) => {
 
     const [subject, setSubject] = useState();
     const [lastClickedButton, setLastClickedButton] = useState(null);
-    const [subjects,setSubjects] = useState()
+    const [subjects,setSubjects] = useState();
+    const [generateAgainClicked, setGenerateAgainClicked] = useState(false)
+
+    const fetchData = async () => {
+        const data = await getSubjects()
+        setSubjects(data)
+    }
     
     useEffect(() => {
-        props.updateMainObj(subject,'')
-    }, [subject])
-
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await getSubjects()
-            setSubjects(data)
-        }
         fetchData()
-    },[])
+        props.updateMainObj(subject,'')
+    }, [])
+
+    const generateAgain = async () => {
+        setGenerateAgainClicked(true)
+        await fetchData()
+        setGenerateAgainClicked(false)
+    }
 
     const renderButtons = () => {
     const buttons = [];
-    for (let i = 0; i < 7; i++) {
-        buttons.push(
-        <button
-            key={i}
-            value={subjects[i]}
-            onClick={(event) => {
-            setSubject(event.target.value);
-            if (lastClickedButton !== null) {
-                lastClickedButton.style.border = "none";
-            }
-            event.target.style.border = "3px solid #946AB3";
-            setLastClickedButton(event.target);
-            }}
-            className={styles.btn}
-        >
-            {subjects[i]}
-        </button>
-        );
-    }
-    return buttons;
-    };
-
-    const renderButtons2 = () => {
-    const buttons = [];
-    for (let i = 7; i < 12; i++) {
+    for (let i = 0; i < 12; i++) {
         buttons.push(
         <button
             key={i}
@@ -79,24 +60,25 @@ const GenaratedSubjects = (props) => {
 
                 <img className={styles.img} src="/procces2.svg" alt="image" />
 
-                <label className={styles.title}>בחירת תחום כללי לנושא</label>
-
-                {subjects && <div className={styles.buttons_container}>
+                {subjects ? <div className={styles.buttons_container}>
+                    <label className={styles.title}>בחירת תחום כללי לנושא</label>
                     <label className={styles.options_label}>הנה כמה אפשרויות שהכנו לך</label>
 
-                    <div className={styles.row1}>
+                    <div className={styles.grid_buttons}>
                         {renderButtons()}
                     </div>
 
-                    <div className={styles.row2}>
-                        {renderButtons2()}
-                    </div>
-
-                    <button className={styles.more_option_btn}>
-                        <label>הצעות נוספות</label>
+                    <button className={styles.more_option_btn} onClick={generateAgain}>
+                        <label className={styles.more_option_btn_label}>הצעות נוספות</label>
+                        {!generateAgainClicked ? 
                         <GoSync></GoSync>
+                        :
+                        <GoSync className={styles.generateAgain_icon_loading}></GoSync>}
                     </button>
-                </div>}
+                </div>
+                    : 
+                    <Loading></Loading>
+                    }
 
             </div>
 
