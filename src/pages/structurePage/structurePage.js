6aa0c61811439b57b27d1e1bc0fb91b2c361dc3b
eyal porat <eyal1.porat@gmail.com>
2024-02-15 15:19:14 +0200

    ui changes

--- src/pages/structurePage/structurePage.js
+++ src/pages/structurePage/structurePage.js
@@ -3,15 +3,17 @@ import Navbar from '../../components/navbar/navbar';
 import UnderButtons from '../../components/underButtons/underButtons';
 import PathData from '../../components/pathData/pathData';
 import { IoIosAlert } from "react-icons/io";
-import { useEffect } from 'react';
-import { createStructure , test } from '../../service/openAIService';
+import { useEffect, useState } from 'react';
+import { getSturctureAnswer } from '../../service/openAIService';
 import LoadingPopUp from '../../components/loadingPopUp/loadingPopUp';
 
 const StructurePage = (props) => {
 
+    const [structure, setStructure] = useState(null);
+
     useEffect(() => {
         const fetchProgramPlanData = async () => {
-            await test()
+            setStructure(await getSturctureAnswer())
         }
         fetchProgramPlanData()
     },[])
@@ -21,7 +23,7 @@ const StructurePage = (props) => {
 
             <img className={styles.img} src="/procces3.svg" alt="image" />
 
-            {props.programPlan.structure ? 
+            {structure ? 
                 <div className={styles.container}>
                     <h2>מתווה למסלול</h2>
                     <label>בנושא {props.programPlan.subject}</label>
@@ -31,14 +33,14 @@ const StructurePage = (props) => {
                         <label>באפשרותכם לערוך את תוכן המתווה</label>
                     </div>
                     
-                    <PathData data = {props.programPlan.structure}/>
+                    <PathData data = {structure}/>
 
                     <img src={props.programPlan.picture} alt='path image'></img>
                 </div>
             :
                 <LoadingPopUp text='אנחנו מכינים לכם מתווה למסלול...'></LoadingPopUp>}
 
-                {props.programPlan.structure && 
+                {structure && 
                     <div className={styles.underBtn}>
                         <UnderButtons text='ליצירת היום הראשון' back='/homePageCreatePath' forward='/day1'/>
                     </div>}
--- src/service/openAIService.js
+++ src/service/openAIService.js
@@ -1,5 +1,38 @@
 import { get, post } from './httpService'
 import axios from 'axios';
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
+
+export async function getSturctureAnswer() {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const prompt = `You are an expert in writing learning paths in different fields for children.
+    Your goal is to write a learning path that is pedagogically adapted for children at the age of 8 in Israel.
+    The path is learned independently, for 5 days, each day for about an hour and a half.
+    Its goal is to empower independent learning, creativity, and critical thinking.
+    The course should work on the following skills: independent learning, creativity, technological literacy, and critical thinking.
+    On the third day, there is a class meeting for half an hour, and on the fifth and final day, there is a class meeting for an hour.
+    You need to build a template of 5 days, with each day having a title, goal (one or two sentences), and headings of the tasks on that day (3-4 tasks). 
+    very important to add ${contentType} to one task per day! (remember that the children at the age of 8 so dont give them something hard). 
+    (if you offer videos add link for that) On the fourth day, there is a task dedicated to preparing the learning presentation in front of the entire class. 
+    Your topic is ${subject}. The path should teach students to understand ${learn} and create a product (give me ideas for types of products in the context of this project. 
+    For example: list, poster, creating an image with an artificial intelligence tool, etc.).
+    During the course, there is no teaching, the students need to understand concepts and acquire knowledge independently through experimentation. Each day should include practical, active work. 
+    dont suggest to watch video or install some apps. 
+    dont suggest to upload text files because the children have a textbox. 
+    Use simple words that children know. Use words that 8 year olds understand. 
+    build missions that 8 years old children can do. 
+    Pay attention to the instructions of the tasks.
+    Emphasize that the tasks will not be similar. give the answer in hebrew!`
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = await response.text();
+    console.log(text)
+    return text
+}
+
 export async function getSubjects() {
     try {
         const subjectList = await get(`OpenAI/get/subjects`)
@@ -56,9 +89,13 @@ export const createStructure = async(structureInfo) => {
 }
 
 
+
+
+// structure in open ai GPT
+
 const axiosInstance = axios.create({
     headers: {
-    'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`,
+    "Authorization": `Bearer ${process.env.REACT_APP_API_KEY}`,
     'Content-Type': 'application/json'
     }
 });
@@ -92,7 +129,6 @@ const requestData = {
     }],
     temperature: 0.7
 };
-
 
 export async function test(params) {
     axiosInstance.post('https://api.openai.com/v1/chat/completions', requestData)
